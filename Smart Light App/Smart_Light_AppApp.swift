bef7cca27b133781e906a951335fb8ef571cb26c
chrslei <86011906+chrslei@users.noreply.github.com>
2023-12-04 12:25:09 +0100

    Initial Commit

---
+++ Smart Light App/Smart_Light_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Light_AppApp.swift
+//  Smart Light App
+//
+//  Created by Christopher Leibiger on 04.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Light_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
